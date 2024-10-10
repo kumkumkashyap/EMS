4797d5c3b853745bea10b59a8097eb508f8531d2
kumkumkashyap <kumkumkashyap.790@icloud.com>
2024-10-10 23:09:28 +0530

    added index.html

---
+++ a.js
@@ -0,0 +1,9 @@
+function b(){
+    var a = confirm("Want to open BMS");
+    if (a == false){
+        
+    }    
+    else{
+        window.open("https://in.bookmyshow.com/","_self");
+    }    
+}
